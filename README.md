16169ac1bcec71b8c3bc02fb26d1034011a10210
asyinka <asyinka@gmail.com>
2022-07-03 10:28:46 +0100

    A guessing game project created with Math functions

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
-This project entails the use of toLocaleString for conversion into Nigeria local currency
+This project entails the use of Math to form a Guessing Game 
+The Game entails guessing the right integer between 1 and 10 with both 1 and 10 inclusive
+Math functions()
--- index.js
+++ index.js
@@ -1,4 +1,25 @@
-value = 19375.9087;
-value = value.toLocaleString("yo-NG", {style: "currency", currency: "NGN"});
+const answer = Math.floor((Math.random() * 9) + 1);
+let guesses = 0;
 
-console.log(value);
+ document.getElementById("submitButton").onclick = function(){
+    
+    let guess = document.getElementById("playerGuess").value;
+    guesses += 1;
+    if (guess == answer) {
+        window.alert(`${guess} is correct, it took you ${guesses} guesses`);
+    } else if (guess > answer ) {
+        window.alert(`${guess} is too large`);
+    } else {
+        alert(`${guess} is too small`);
+    }
+ }
+
+
+
+
+
+
+// value = .5;
+// value = value.toLocaleString(undefined, {style: "unit", unit: "celsius"});
+
+// console.log(value);
