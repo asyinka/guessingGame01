98f2420ae1ccb67f7caeb9a20c9f6bcc390f6c61
asyinka <asyinka@gmail.com>
2022-07-03 09:29:29 +0100

    a simple toLocaleString for currency conversion in Nigeria currency

---
+++ README.md
@@ -0,0 +1,1 @@
+This project entails the use of toLocaleString for conversion into Nigeria local currency
---
+++ index.js
@@ -0,0 +1,4 @@
+value = 19375.9087;
+value = value.toLocaleString("yo-NG", {style: "currency", currency: "NGN"});
+
+console.log(value);
